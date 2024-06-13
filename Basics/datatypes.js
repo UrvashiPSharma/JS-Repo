557cab27273f7ebe1dced45aa362511d252320f3
UrvashiPSharma <154873649+UrvashiPSharma@users.noreply.github.com>
2024-06-13 07:18:43 +0000

    practice doc of datatypes in js

---
+++ Basics/datatypes.js
@@ -0,0 +1,16 @@
+console.log("hello")
+let name="harish"
+const id=0
+console.log(typeof id)
+console.log(Number(id))
+let isCaps=undefined
+console.log(typeof isCaps)
+/* Practicing datatypes*/
+
+bol=Boolean(isCaps)
+console.log("converting null to bool" ,bol)
+
+let value=null
+console.log("type of null" ,typeof(value))
+name1="urvashi"
+console.table([id,isCaps, value,name1])
